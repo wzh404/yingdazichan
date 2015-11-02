d87a373aa483b65cb481f7ff9306862fcc4dff75
wangzunhui <wzh404@hotmail.com>
2015-11-02 23:33:34 +0800

    change name or moble

--- src/main/java/com/xeehoo/p2p/service/impl/LoanStaffServiceImpl.java
+++ src/main/java/com/xeehoo/p2p/service/impl/LoanStaffServiceImpl.java
@@ -10,10 +10,8 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by wangzunhui on 2015/11/2.
@@ -31,6 +29,7 @@ public class LoanStaffServiceImpl implements LoanStaffService {
     @Override
     public Map<String, Object> staffLogin(String staffName, String staffPwd) {
         Map<String, Object> map = new HashMap<String, Object>();
+
         map.put("result", Constant.RESULT_ERROR);
         map.put("resultMsg", "用户不存在");
 
@@ -41,15 +40,18 @@ public class LoanStaffServiceImpl implements LoanStaffService {
                 return staff.getStaffPwd().equalsIgnoreCase(staffPwd);
             })
             .map((staff) -> {
-                logger.info("-------check permission----");
-                List<LoanPermission> perms = permissionMapper.getPermissionByRoleCode(staff.getStaffRole());
-                return perms;
+                //logger.info("-------check permission----");
+                return permissionMapper.getPermissionByRoleCode(staff.getStaffRole());
             })
-            .ifPresent((perms) -> {
-                for (LoanPermission perm : perms) {
+           .ifPresent((perms) -> {
+               List<LoanPermission> rolePerms = perms.stream()
+                       .filter((perm) -> perm.getPermissionId() > 0)
+                       .collect(Collectors.toList());
 
-                };
-            });
+               Map<String, List<LoanPermission>> menu  = perms.stream()
+                       .filter((perm) -> perm.getPermissionId() == 0)
+                       .collect(Collectors.groupingBy(LoanPermission::getMenuCode));
+           });
         logger.info("---------return--------");
         return map;
     }
