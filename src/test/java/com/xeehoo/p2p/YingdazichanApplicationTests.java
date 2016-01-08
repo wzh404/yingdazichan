d97c79cf5e0f93575259a6b525e0d57147071e86
wangzunhui <wangzunhui@neowave.cn>
2016-01-08 18:01:33 +0800

    add query conditions

--- src/test/java/com/xeehoo/p2p/YingdazichanApplicationTests.java
+++ src/test/java/com/xeehoo/p2p/YingdazichanApplicationTests.java
@@ -4,6 +4,7 @@ import com.sun.istack.internal.logging.Logger;
 import com.xeehoo.p2p.mybatis.mapper.UserMapper;
 import com.xeehoo.p2p.po.LoanUser;
 import com.xeehoo.p2p.util.IDCard;
+import com.xeehoo.p2p.util.InterestUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,16 +34,20 @@ public class YingdazichanApplicationTests {
 
 	@Test
 	public void saveUser(){
-        LoanUser userinfo = new LoanUser();
-
-        userinfo.setLoginName("abcd1");
-        java.util.Date regtime = new java.util.Date(System.currentTimeMillis());
-        userinfo.setRegisterTime(regtime);
-        userinfo.setRegisterIP("127.0.0.1");
-        userinfo.setMobile("13501185106");
-        userinfo.setLoginPwd(userinfo.encryptPwd("123456"));
-        userinfo.setUserStatus(0x01);
-        userMapper.saveUser(userinfo);
-        assert(userinfo.getUserId() > 0);
+//        LoanUser userinfo = new LoanUser();
+//
+//        userinfo.setLoginName("abcd1");
+//        java.util.Date regtime = new java.util.Date(System.currentTimeMillis());
+//        userinfo.setRegisterTime(regtime);
+//        userinfo.setRegisterIP("127.0.0.1");
+//        userinfo.setMobile("13501185106");
+//        userinfo.setLoginPwd(userinfo.encryptPwd("123456"));
+//        userinfo.setUserStatus(0x01);
+//        userMapper.saveUser(userinfo);
+//        assert(userinfo.getUserId() > 0);
+		Date d = new Date();
+		Date c = new Date(116, 2, 7);
+        System.out.println(c.toString());
+		InterestUtil.getStages(d, c);
 	}
 }
