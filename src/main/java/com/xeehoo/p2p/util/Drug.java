592a3c0f89be672a506cd01507e1202acec9fe97
wangzunhui <wangzunhui@neowave.cn>
2016-05-06 16:35:12 +0800

    add query conditions

---
+++ src/main/java/com/xeehoo/p2p/util/Drug.java
@@ -0,0 +1,126 @@
+package com.xeehoo.p2p.util;
+
+import java.util.Date;
+
+/**
+ * Created by wangzunhui on 2016/5/6.
+ */
+public class Drug {
+    private Integer id;
+    private String pzwh; //批准文号
+    private String cpmc; //产品名称
+    private String ywmc; // 英文名称
+    private String spm; //商品名
+    private String jx; // 剂型
+    private String gg; // 规格
+    private String scdw; // 生产单位
+    private String scdz; //生产地址
+    private String cplb; // 产品类别
+    private String ypzwh; //原批准文号
+    private String pzrq;  // 批准日期
+    private String ypbwm; //药品本位码
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPzwh() {
+        return pzwh;
+    }
+
+    public void setPzwh(String pzwh) {
+        this.pzwh = pzwh;
+    }
+
+    public String getCpmc() {
+        return cpmc;
+    }
+
+    public void setCpmc(String cpmc) {
+        this.cpmc = cpmc;
+    }
+
+    public String getYwmc() {
+        return ywmc;
+    }
+
+    public void setYwmc(String ywmc) {
+        this.ywmc = ywmc;
+    }
+
+    public String getSpm() {
+        return spm;
+    }
+
+    public void setSpm(String spm) {
+        this.spm = spm;
+    }
+
+    public String getJx() {
+        return jx;
+    }
+
+    public void setJx(String jx) {
+        this.jx = jx;
+    }
+
+    public String getGg() {
+        return gg;
+    }
+
+    public void setGg(String gg) {
+        this.gg = gg;
+    }
+
+    public String getScdw() {
+        return scdw;
+    }
+
+    public void setScdw(String scdw) {
+        this.scdw = scdw;
+    }
+
+    public String getScdz() {
+        return scdz;
+    }
+
+    public void setScdz(String scdz) {
+        this.scdz = scdz;
+    }
+
+    public String getCplb() {
+        return cplb;
+    }
+
+    public void setCplb(String cplb) {
+        this.cplb = cplb;
+    }
+
+    public String getYpzwh() {
+        return ypzwh;
+    }
+
+    public void setYpzwh(String ypzwh) {
+        this.ypzwh = ypzwh;
+    }
+
+    public String getPzrq() {
+        return pzrq;
+    }
+
+    public void setPzrq(String pzrq) {
+        this.pzrq = pzrq;
+    }
+
+    public String getYpbwm() {
+        return ypbwm;
+    }
+
+    public void setYpbwm(String ypbwm) {
+        this.ypbwm = ypbwm;
+    }
+}
